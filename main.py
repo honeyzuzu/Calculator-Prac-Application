b5f4ffc509990fa4820f76231b9873314b3f5e15
Queenie Sun <91141179+honeyzuzu@users.noreply.github.com>
2022-12-08 00:33:39 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+Calc_Prac = Tk()
+root = Tk()
+
+#setting the width x height
+root.geometry("312x324")
+
+#stop root from being resized
+root.resizable(0,0)
+
+#titling the window of the calc
+root.title("Calculator!!!!!!!!")
+
+
